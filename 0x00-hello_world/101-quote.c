be48c28155c93cd559012df54a89a9e3bd8670ce
veera-2 <naveera.nh@gmail.com>
2023-09-07 15:45:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - this prints exactly the input on it
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
